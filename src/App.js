0eef4685f5691de5785fe587808ed01476412928
Kevin <kevcespedesarancibia87@gmail.com>
2021-09-02 10:09:16 -0400

    Calculate taxes and show total with taxes

--- src/App.js
+++ src/App.js
@@ -1,83 +1,62 @@
 const price = document.querySelector("#price-input");
 const quantity = document.querySelector("#quantity-input");
-const quantity_price = document.querySelector("#show-quantity-price");
+const total = document.querySelector("#show-total");
+const total_with_taxes = document.querySelector("#show-total-with-taxes");
+const name_item = document.querySelector("#product-input");
+
+const name_output= document.querySelector("#product-output");
+const quantity_output= document.querySelector("#quantity-output");
+const price_output= document.querySelector("#price-output");
+
+const state_input= document.querySelector("#State");
+
 const form = document.querySelector("#totalizer-form");
 
-/*for (i=1; i<=6; i++) {
-    let x = "<input type='text' id='quantityP"+i+"'/><input type='text' id='priceP"+i+"'/><br>";
-    document.getElementById("products").innerHTML = x;
-}*/
-/*
+var taxes = {
+    "UT":0.0665,
+    "NV":0.08,
+    "TX":0.0625,
+    "AL":0.04,
+    "CA":0.0825
+}
 
-document.getElementById("demo").innerHTML = "Hello JavaScript!
-*/
 
 form.addEventListener("submit",event=>{
     event.preventDefault();
-    x = "La cantidad " + quantity.value + ", el precio " + price.value + ": " + (price.value*quantity.value);
-    alert(x);
-    quantity_price.innerHTML = x;
+    let total_price = (price.value * quantity.value)
+    total.innerHTML = "Total: " + total_price;
+    name_output.innerHTML = name_item.value;
+    quantity_output.innerHTML = quantity.value;
+    price_output.innerHTML = price.value;
+    alert ("State Selected: " + state_input.value + " tax: " + taxes[state_input.value] );//TAXES
+    let total_with_taxes_var = total_price +taxes[state_input.value] * total_price;
+    total_with_taxes.innerHTML = "Total with taxes: "+ total_with_taxes_var
+    // alert ( "Precio total con descuento: " + total_withDiscount );
 })
 
 /*
-function greetingSelector(name,age,gender){
-    var greetings = {
-        0:{
-            "male":"Hola amigo",
-            "female":"Hola amiga"
-        },
-        1:{
-            "male":"Que tal",
-            "female":"Que tal"
-        },
-        2:{
-            "male":"Como esta Don",
-            "female":"Como esta Sra."
-        }
+function tableCreate() {
+  var body = document.getElementsByTagName('body')[0];
+  var tbl = document.createElement('table');
+  tbl.style.width = '100%';
+  tbl.setAttribute('border', '1');
+  var tbdy = document.createElement('tbody');
+  for (var i = 0; i < 3; i++) {
+    var tr = document.createElement('tr');
+    for (var j = 0; j < 2; j++) {
+      if (i == 2 && j == 1) {
+        break
+      } else {
+        var td = document.createElement('td');
+        td.appendChild(document.createTextNode('\u0020'))
+        i == 1 && j == 1 ? td.setAttribute('rowSpan', '2') : null;
+        tr.appendChild(td)
+      }
     }
-    var greetings_by_time = {
-        0:"Buen día",
-        1:"Buenas tardes",
-        2:"Buena noche"
-    }
-    var limits = [20,45]
-    group = valueIntervalFinder(age,limits);
-    var current_time = new Date(); 
-    var time_limits = [new Date().setHours(12,0),new Date().setHours(18,0)]
-    time_group = valueIntervalFinder(current_time,time_limits)
-    //alert(current_time)
-    return greetings[group][gender] + " " + name + ". "+greetings_by_time[time_group]+"."
+    tbdy.appendChild(tr);
+  }
+  tbl.appendChild(tbdy);
+  body.appendChild(tbl)
 }
-
-function isAnIndex(index,lenght){
-    var ans = false;
-    if (index<lenght && index>0){
-        ans = true;
-    }
-    return ans;
-}
-
-function valueIntervalFinder(value,limits)
-{
-    var interval;
-    if (value<limits[0])
-    {
-        interval = 0
-    }
-    else if (value >limits[limits.length-1])
-    {
-        interval = limits.length
-    }
-    else
-    {
-        for(var i=0;i<limits.length;i=i+1)
-        {
-            if (limits[i]<=value && value<=limits[i+1])
-            {
-                interval = i + 1;
-            }
-        }
-    }
-    return interval;
-}
+tableCreate();
 */
